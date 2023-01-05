ef53290c3bab690f2733bc14427d0b9880cc6b4e
Dmity Sharapov <mrneey@gmail.com>
2023-01-05 12:40:47 +0300

    вывод страницы регистрации

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import { ClientErrorPage } from "./pages/error/error";
-import { SignInPage } from "./pages/signIn/signIn";
 import { SignUpPage } from "./pages/signUp/signUp";
-import { ChatsPage } from "./pages/chats/chats";
-import { ProfilePage } from "./pages/profile/profile";
-import { ChangeDataProfilePage } from "./pages/changeDataProfile/changeDataProfile";
-import { ChangePasswordProfilePage } from "./pages/changePasswordProfile/changePasswordProfile";
 
 console.log("entry point");
 
-document.getElementById("root").innerHTML = ChangePasswordProfilePage();
+document.getElementById("root").innerHTML = SignUpPage();
