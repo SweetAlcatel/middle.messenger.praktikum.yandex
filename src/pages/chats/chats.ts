45fcbf3e5fbd333918a35f2f37f9ff87b83abf9f
Dmity Sharapov <mrneey@gmail.com>
2023-02-14 18:45:01 +0300

    рефакторинг профиля

---
+++ src/pages/chats/chats.ts
@@ -0,0 +1,3 @@
+import { Block } from "../../utils/block";
+import chatsTemplate from "bundle-text:./chats.hbs";
+import { SelectedChat } from "../../layout/selectedChat/selectedChat";
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,1 +1,54 @@
 import { Block } from "../../utils/block";
+import profileTemplate from "bundle-text:./profile.hbs";
+import { Button } from "../../layout/button/button";
+import { Field } from "../../layout/field/field";
+import AuthController from "../../controllers/authController";
+import { FixMeLater } from "../../types";
+import { withStore } from "../../utils/store";
+
+const userFields = [
+  "id",
+  "first_name",
+  "second_name",
+  "display_name",
+  "login",
+  "avatar",
+  "email",
+  "phone",
+];
+
+class ProfilePageBase extends Block<FixMeLater> {
+  init() {
+    this.children.fields = userFields.map((name) => {
+      return new Field({ name, value: this.props[name] });
+    });
+
+    this.children.logoutButton = new Button({
+      text: "Выйти",
+      events: {
+        click: () => {
+          AuthController.logout();
+        },
+      },
+    });
+  }
+
+  protected componentDidUpdate(
+    oldProps: FixMeLater,
+    newProps: FixMeLater
+  ): boolean {
+    (this.children.fields as FixMeLater[]).forEach((field, i) => {
+      field.setProps({ value: newProps[userFields[i]] });
+    });
+
+    return false;
+  }
+
+  render() {
+    return this.compile(profileTemplate, this.props);
+  }
+}
+
+const withUser = withStore((state) => ({ ...state.user }));
+
+export const ProfilePage = withUser(ProfilePageBase);
